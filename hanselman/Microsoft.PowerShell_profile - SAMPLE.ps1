6419979da7d405bac72b35a62650badc0e3389b5
Iristyle <Iristyle@Github>
2013-05-09 11:41:39 -0400

    chore: original Hanselman source files

---
+++ hanselman/Microsoft.PowerShell_profile - SAMPLE.ps1
@@ -0,0 +1,27 @@
+function Get-Batchfile ($file) {
+    $cmd = "`"$file`" & set"
+    cmd /c $cmd | Foreach-Object {
+        $p, $v = $_.split('=')
+        Set-Item -path env:$p -value $v
+    }
+}
+
+function VsVars32($version = "10.0")
+{
+    $key = "HKLM:SOFTWARE\Microsoft\VisualStudio\" + $version
+    $VsKey = get-ItemProperty $key
+    $VsInstallPath = [System.IO.Path]::GetDirectoryName($VsKey.InstallDir)
+    $VsToolsDir = [System.IO.Path]::GetDirectoryName($VsInstallPath)
+    $VsToolsDir = [System.IO.Path]::Combine($VsToolsDir, "Tools")
+    $BatchFile = [System.IO.Path]::Combine($VsToolsDir, "vsvars32.bat")
+    Get-Batchfile $BatchFile
+    [System.Console]::Title = "Visual Studio " + $version + " Windows Powershell"
+    Set-ConsoleIcon "C:\SOMEWHEREYOUDECIDE\WindowsPowershell\vspowershell.ico"
+}
+
+Push-Location (Split-Path -Path $MyInvocation.MyCommand.Definition -Parent)
+
+# DotSource the Console Icon Stuff
+. ./Set-ConsoleIcon.ps1
+
+Pop-Location
